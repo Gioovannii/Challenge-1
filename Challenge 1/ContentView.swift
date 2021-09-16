66ef6ececa2ab702852faf5303622aa08e8cfc76
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-16 11:07:24 +0800

    Initial Commit

---
+++ Challenge 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge 1
+//
+//  Created by Giovanni Gaffé on 2021/9/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
