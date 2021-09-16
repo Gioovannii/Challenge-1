9056fd4de3a1f6183f7d81e48b6450a7f4544ea4
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-16 11:25:09 +0800

    Set ui

--- Challenge 1/ContentView.swift
+++ Challenge 1/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput = ""
+    @State private var convertMesure = ""
+    @State private var resultOutput = ""
+    
+    @State private var selectedValue = 1
+    @State private var selectedValueTarget = 1
+
+    let convertedValues = ["Celcius", "Fahrenheit", "Kelvin"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Hello, world!", text: $userInput)
+                Text("User input : \(userInput)")
+            }
+            
+            Section {
+                Picker("selectedValue", selection: $selectedValue) {
+                    ForEach(0 ..< convertedValues.count) {
+                        Text("\(self.convertedValues[$0])")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section {
+                Picker("selectedValue", selection: $selectedValueTarget) {
+                    ForEach(0 ..< convertedValues.count) {
+                        Text("\(self.convertedValues[$0])")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section {
+                Text("Your conversion is \(resultOutput)")
+            }
+        }
+        
     }
 }
 
